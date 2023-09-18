84defb57c35a171a072155d4c3703cc5bdbf78ab
moises.cantillo <moicanbas@gmail.com>
2023-09-18 14:01:34 -0500

    first commit
    first commit

---
+++ src/layout/dashboard/ContentFooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <div class="copyright">
+        © {{year}} hecho con el <i class="tim-icons icon-heart-2"></i> by
+        <a href="https://github.com/moicanbas" target="_blank" rel="noopener">Moisés cantillo,</a> Eder Castro &nbsp;&
+        <a href="https://github.com/isradeveloper" target="_blank" rel="noopener">Israel trujillo</a> para desarrollo web II.
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        year: new Date().getFullYear()
+      }
+    }
+  };
+</script>
+<style>
+</style>
---
+++ src/layout/dashboard/DashboardLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="wrapper">
+    <side-bar>
+      <template slot="links">
+        <sidebar-link to="/" name="Inicio" icon="tim-icons icon-chart-pie-36"/>
+        <sidebar-link to="/carrito-compras" name="Carrito" icon="tim-icons icon-cart"/>
+        <sidebar-link to="/historial-pedidos" name="Histórico pedidos" icon="tim-icons icon-single-copy-04"/>
+        <sidebar-link to="/profile" name="Datos de facturación" icon="tim-icons icon-delivery-fast"/>
+      </template>
+    </side-bar>
+    <div class="main-panel">
+      <top-navbar></top-navbar>
+
+      <dashboard-content @click.native="toggleSidebar">
+
+      </dashboard-content>
+
+      <content-footer></content-footer>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+</style>
+<script>
+import TopNavbar from "./TopNavbar.vue";
+import ContentFooter from "./ContentFooter.vue";
+import DashboardContent from "./Content.vue";
+import MobileMenu from "./MobileMenu";
+export default {
+  components: {
+    TopNavbar,
+    ContentFooter,
+    DashboardContent,
+    MobileMenu
+  },
+  methods: {
+    toggleSidebar() {
+      if (this.$sidebar.showSidebar) {
+        this.$sidebar.displaySidebar(false);
+      }
+    }
+  }
+};
+</script>
